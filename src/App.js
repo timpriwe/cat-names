6da044bef9fdb0a81c6fe7520914ed93c6e5887f
Tim Priwe <tim.priwe@gmail.com>
2018-05-16 10:15:12 +0200

    added function to show random cat names, changed README.md

--- src/App.js
+++ src/App.js
@@ -4,47 +4,48 @@ import Cat from './Cat';
 
 class App extends Component {
 
+  constructor(props) {
+    super(props);
 
-constructor(){
-super();
-
-this.getRandomCatNames = this.getRandomCatNames.bind(this);
-
-// initial state
-this.state = {
-  cats: sampleCats,
-  catsToDisplay: []
+    // initial state
+    this.state = {
+      cats: sampleCats,
+      catsToDisplay: [],
+    }
   }
-}
 
-getRandomCatNames(source, neededNumber){
+  static getRandomCatNames(cats, catsCount){
+    const catNames =
+      Object
+        .keys(cats)
+        .map(key => cats[key].name);
 
-  const shuffled = source.slice().sort(() => .5 - Math.random()); // shuffle  
-  const selected = shuffled.slice(0, neededNumber); //get sub-array of first n elements AFTER shuffle
+    const shuffledNames = catNames.slice().sort(() => .5 - Math.random()); // shuffle  
+    return shuffledNames.slice(0, catsCount); //get sub-array of first n elements AFTER shuffle
+  } 
 
-  return selected;
+  componentDidMount(){
+    if (this.props.match && this.props.match.params && this.props.match.params.catCount) {
+      this.setState({
+        catsToDisplay:
+        App.getRandomCatNames(this.state.cats, this.props.match.params.catCount)
+      });
+    }
+  }
 
-  /*
-  let result = [];
-  for (var i = 0; i < neededNumber; i++) {
-  result.push(source[Math.floor(Math.random()*source.length)]);
-}
-return result;
-*/
-} 
+  renderRandomCats() {
+    return (
+     this.state.catsToDisplay.map(name => <div key={name}>{name}</div>)
+    );
+  }
 
-componentDidMount(){
-
-  this.setState(
-
-    {catsToDisplay: this.getRandomCatNames(cats, this.props.match.params.catCount)}
-    
-  )
-
-  
-}
-
-
+  renderCats() {
+    return (
+      Object
+        .keys(this.state.cats)
+        .map(key => <Cat key={key} index={key} details={this.state.cats[key]} />)
+    );
+  }
 
   render() {
     return (
@@ -52,9 +53,9 @@ componentDidMount(){
         <h2><span role="img" aria-label="loving cat">😻</span> Let's See Some Cats <span role="img" aria-label="loving cat">😻</span></h2>
         <ul className="list-of-cats">
             {
-              Object
-                .keys(this.state.cats)
-                .map(key => <Cat key={key} index={key} details={this.state.cats[key]}/>)
+              this.state.catsToDisplay.length > 0
+              ? this.renderRandomCats()
+              : this.renderCats()
             }
         </ul>
       </div>
